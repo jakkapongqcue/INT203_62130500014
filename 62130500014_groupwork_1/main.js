2671ecc884824bae0fd4d8a5a16e755529d11188
Jakkapong Praidtthanachot <jakkapong.q@mail.kmutt.ac.th>
2021-01-30 13:17:39 +0700

    [update]

--- 62130500014_groupwork_1/main.js
+++ 62130500014_groupwork_1/main.js
@@ -3,7 +3,46 @@ const app = {
         return {
             msg: 'Hello Hercules'
         }
-    }
+    },
+    data() {
+        return {
+            lang: {
+                support: 'การสนับสนุน',
+                download: 'ดาวน์โหลด',
+                singUP: 'ลงทะเบียน',
+                logIn: 'เข้าสู่ระบบ',
+                comment: 'เพลงและพอดแคสต์หลายล้านรายการ ไม่ต้องใช้บัตรเครดิต',
+                midButton: 'ใช้ SPOTIFY FREE '
+            }
+        }
+    },
+    methods: {
+        clickMe() {
+            alert("กำลังเปืด https://www.spotify.com/th-th/");
+        },
+        eng() {
+            this.lang = {
+                support: 'Support',
+                download: 'Download',
+                singUP: 'Sing up',
+                logIn: 'log in',
+                comment: 'Millions of songs and podcasts. No credit card needed.',
+                midButton: 'GET SPOTIFY FREE '
+            }
+
+        },
+        thai() {
+            this.lang = {
+                support: 'การสนับสนุน',
+                download: 'ดาวน์โหลด',
+                singUP: 'ลงทะเบียน',
+                logIn: 'เข้าสู่ระบบ',
+                comment: 'เพลงและพอดแคสต์หลายล้านรายการ ไม่ต้องใช้บัตรเครดิต',
+                midButton: 'ใช้ SPOTIFY FREE '
+            }
+        }
+    },
+
 }
 
 Vue.createApp(app).mount('#app')
