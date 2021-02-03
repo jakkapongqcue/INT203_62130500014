5ed3e6eedd3e872e169487744571a3cdb7cdd2de
Jakkapong Praidtthanachot <jakkapong.q@mail.kmutt.ac.th>
2021-02-03 11:54:15 +0700

    [update v1]

---
+++ 62130500014_groupwork_2/profile.js
@@ -0,0 +1,14 @@
+const app = {
+    data() {
+        return {
+            msg: 'Hello ,Vue3',
+            shown: true,
+            url: 'https://www.sit.kmutt.ac.th/',
+            name: 'Jakkapong Parditthanachot',
+            img: 'preview.jpg',
+            wallpaper: 'https://images.unsplash.com/photo-1593642634402-b0eb5e2eebc9?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+        }
+    },
+}
+
+var mountedApp = Vue.createApp(app).mount('#app')
