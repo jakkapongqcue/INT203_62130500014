6907ee42992840fdaf2722fd510d530a965ab85b
Jakkapong Praidtthanachot <jakkapong.q@mail.kmutt.ac.th>
2021-02-03 12:04:53 +0700

    [update v2]

--- 62130500014_groupwork_2/profile.js
+++ 62130500014_groupwork_2/profile.js
@@ -5,7 +5,7 @@ const app = {
             shown: true,
             url: 'https://www.sit.kmutt.ac.th/',
             name: 'Jakkapong Parditthanachot',
-            img: 'preview.jpg',
+            img: 'profile.jpg',
             wallpaper: 'https://images.unsplash.com/photo-1593642634402-b0eb5e2eebc9?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
         }
     },
